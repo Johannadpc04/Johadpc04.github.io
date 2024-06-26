047f2d525f15ca405c58ce0d63223c2db847353c
Johannadpc04 <johanna.dpc@gmail.com>
2024-06-25 20:48:18 -0500

    Create script.js

---
+++ unidad_01/basic/04_taller/js/script.js
@@ -0,0 +1,17 @@
+function cargar_elemento(){
+    var content= document.getElementById("content")
+    var texto=document.getElementById("texto")
+
+    var div=document.createElement("div")
+    div.className="item-box"
+    var texto=document.createTextNode(texto.value)
+    contador++
+    div.appendChild(texto)
+
+    content.appendChild(div)
+}
+
+function eliminar_elemento(){
+    var content = document.getElementById("content")
+    content.removeChild(content.lastElementChild)
+}
