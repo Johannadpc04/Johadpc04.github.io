818e7b6c5e42e2fa8b2efb2d10c5728a3b65ada6
Johannadpc04 <johanna.dpc@gmail.com>
2024-06-29 12:07:24 -0500

    Update script.js

--- unidad_01/intermediate/04_taller/script.js
+++ unidad_01/intermediate/04_taller/script.js
@@ -1,3 +1,18 @@
+//duncion declarativa
 function imprimir(){
-    console.log 
+    console.log('Hola mundo.')
 }
+
+imprimir()
+
+//funcion expresiva o tambien denominada como anonima
+const suma=function(a, b){
+    return(a + b)
+}
+console.log( suma(10,20) )
+
+//funcion flecha
+const suma2=(a, b)=>{
+
+}
+
