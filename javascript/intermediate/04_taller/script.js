ab5f61588040a44933944f1c5df8ac6a4eb0a3ae
Johannadpc04 <johanna.dpc@gmail.com>
2024-07-02 15:33:32 -0500

    Create script.js

---
+++ javascript/intermediate/04_taller/script.js
@@ -0,0 +1,25 @@
+//duncion declarativa
+function imprimir(){
+    console.log('Hola mundo.')
+}
+
+imprimir()
+
+//funcion expresiva o tambien denominada como anonima
+const suma=function(a, b){
+    return(a + b)
+}
+console.log( suma(10,20) )
+
+//funcion flecha
+const suma2 = (a, b)=>{
+    let total = a + b
+    console.log(` la suma de ${a} y ${b} es ${total}.`)
+} 
+
+suma2(100,200)
+
+const suma3 = (a, b) => a + b
+
+console.log( suma3(10000, 2000) )
+
