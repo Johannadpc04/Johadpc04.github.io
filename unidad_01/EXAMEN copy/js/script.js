c10d0af9ab8bb780f66bd480bd21e5dccdbccb88
Johannadpc04 <johanna.dpc@gmail.com>
2024-07-06 19:39:10 -0500

    actualizacion

--- unidad_01/EXAMEN copy/js/script.js
+++ unidad_01/EXAMEN copy/js/script.js
@@ -12,13 +12,15 @@ function seleccionar_opcion(opcion){
     }
     
 }
+
 function cargar_elemento() {
     var content = document.getElementById("content")
     var texto = document.getElementById("texto")
     
     var div = document.createElement("div")
-    div.className = "item-box"
-    var texto = document.createTextNode( texto.value )
+    div.className="item-box"
+
+    var texto =document.createTextNode(texto.value)
     div.appendChild(texto)
 
     content.appendChild(div)
