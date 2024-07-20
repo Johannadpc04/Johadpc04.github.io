14ad16ab984b9192eccac960933b2cc27f8969e2
Johannadpc04 <johanna.dpc@gmail.com>
2024-07-20 12:47:28 -0500

    act

--- javascript/advanced/ejercicio_01/script.js
+++ javascript/advanced/ejercicio_01/script.js
@@ -2,17 +2,23 @@ const URL="https://rickandmortyapi.com/api/character/"
 var xhttp= null
 
 function conectar(personaje){
-    xhttp= new XMLHttpRequest ()
+    xhttp= new XMLHttpRequest()
     xhttp.onreadystatechange=obtener_respuesta
-    xhttp.open('GET', URL + personaje)
+    xhttp.open('GET', URL + personaje, true)
     xhttp.send()
 }
 
 function obtener_respuesta(){
-    if(xhttp != null && xhttp.readystate==4){
+    if(xhttp != null && xhttp.readyState==4){
         if(xhttp.status == 200){
             let dato= JSON.parse(xhttp.responseText)
-            console.log(xhttp.responseText)
+            //console.log(xhttp.responseText)
+            console.log(dato)
+
+            let container= document.getElementById('container')
+            let personaje=`<h1>${dato.name}</h1>
+            <img src="${dato.image}" />`
+            container.innerHTML=personaje
         }
     }
 }
