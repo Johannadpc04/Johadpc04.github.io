161128419a4d7c8de5644305f482eb955216e82e
Johannadpc04 <johanna.dpc@gmail.com>
2024-09-03 23:35:58 -0500

    TAREA

--- unidad_04/U4_PRACTICA/configuracion.js
+++ unidad_04/U4_PRACTICA/configuracion.js
@@ -1,10 +1,6 @@
 const config = {
     PUERTO: 3000,
-    /*DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',*/
-    //DB_URL:'mongodb+srv://johannadpc:johanna2004DP@cluster0.yg6h7.mongodb.net/',
     DB_URL:'mongodb+srv://johannadpc:johanna2004DP@cluster0.yg6h7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    //DB_URL:'mongodb://localhost:27017/'
-    //DB_URL:'mongodb+srv://johannadpc:johanna2004DP@cluster0.yg6h7.mongodb.net/',
 }
 
 module.exports = config
